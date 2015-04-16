84e37c8d19d9f6bc05000ae7d6f6d3aa4fcd0e04
SudharshanaSL <15.hari.hari@gmail.com>
2015-04-16 23:28:52 +0530

    Create gamesbyseries.php
    Fetch games by series name

---
+++ gamesbyseries.php
@@ -0,0 +1,41 @@
+<?php
+    $key=$_GET['key'];
+    $array = array();
+    $con=mysql_connect("localhost","root","");
+    $db=mysql_select_db("vg_db",$con);
+    $query=mysql_query("select Name from Series where Name LIKE '%{$key}%'");
+    while($row=mysql_fetch_assoc($query))
+    {
+      $array[] = $row['Name'];
+    }
+    echo json_encode($array);
+
+
+
+    if(isset($_POST['submit'])){
+  {
+  //connect  to the database
+  $db=mysql_connect  ("localhost", "root",  "") or die ('I cannot connect to the database  because: ' . mysql_error());
+  //-select  the database to use
+  $mydb=mysql_select_db("vg_db");
+  //-query  the database table
+  $sql="Select Name,Nth_game,Awards,Rating,Year from Video_games where V_ID in (select V_ID from Part_of where S_ID in(select S_ID from Series where Name rlike '%{$name}%' ))";
+  //-run  the query against the mysql query function
+  $result=mysql_query($sql);
+  //-create  while loop and loop through result set
+  while($row=mysql_fetch_array($result)){
+          $Name  =$row['Name'];
+          $Nth_game =$row['Nth_game'];
+          $Awards =$row['Awards'];
+          $Year=$row['Year'];
+          $Rating=$row['Rating'];
+  //-display the result of the array
+  echo "<ul>\n";
+  echo "<li>"    .$Name . " " . $Nth_game. " " . $Awards . " " . $Rating . " " . $Year .  "</a></li>\n";
+  echo "</ul>";
+  }
+  
+  
+  }
+  }
+?>
